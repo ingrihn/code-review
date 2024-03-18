a54ebd849e190dcbe85bdd112ec3a95ead1c40f6
KC <kcesmena@stud.ntnu.no>
2024-03-18 14:24:58 +0100

    Make some functions async for performance (#7)

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,6 @@ import {
 } from "vscode";
 
 import path from "path";
-import readline from "readline";
 
 interface CommentType {
   fileName: string;
@@ -34,24 +33,13 @@ interface CommentType {
 
 let panel: WebviewPanel;
 let activeEditor: TextEditor;
-let iconDecoration: DecorationOptions[] = [];
-let highlightDecoration: DecorationOptions[] = [];
 let icon: TextEditorDecorationType;
 let highlight: TextEditorDecorationType;
-let comments: CommentType[] = [];
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
   activeEditor = window.activeTextEditor ?? window.visibleTextEditors[0];
-  icon = window.createTextEditorDecorationType({
-    after: {
-      contentIconPath: context.asAbsolutePath(path.join("src", "comment.svg")),
-      margin: "5px",
-    },
-  });
-  highlight = window.createTextEditorDecorationType({
-    backgroundColor: "#8CBEB260",
-  });
-  getComments();
+  const filePath = getFilePath("comments.json");
+  await showFileComments(filePath, context);
 
   context.subscriptions.push(
     commands.registerCommand("extension.showCommentSidebar", () => {
@@ -68,9 +56,9 @@ export function activate(context: ExtensionContext) {
       const cssPath = Uri.joinPath(context.extensionUri, "src", "custom.css");
       const cssUri = panel.webview.asWebviewUri(cssPath);
 
-      fs.readFile(webviewPath, "utf-8", (err, data) => {
-        if (err) {
-          window.showErrorMessage(`Error reading HTML file: ${err.message}`);
+      fs.readFile(webviewPath, "utf-8", (error, data) => {
+        if (error) {
+          window.showErrorMessage(`Error reading HTML file: ${error.message}`);
           return;
         }
 
@@ -88,10 +76,10 @@ export function activate(context: ExtensionContext) {
   );
 
   context.subscriptions.push(
-    window.onDidChangeActiveTextEditor((editor: TextEditor | undefined) => {
+    window.onDidChangeActiveTextEditor((editor) => {
       if (editor) {
         activeEditor = editor;
-        getComments();
+        showFileComments(filePath, context);
       }
     })
   );
@@ -100,28 +88,82 @@ export function activate(context: ExtensionContext) {
 function handleMessageFromWebview(message: any) {
   switch (message.command) {
     case "getText":
-      const { text: commentText } = message;
+      const filePath = getFilePath("comments.json");
       const fileName = activeEditor.document.fileName;
-      saveToFile(fileName, activeEditor.selection, commentText);
+      const { commentText } = message;
+      saveToFile(filePath, fileName, activeEditor.selection, commentText);
       deactivate();
   }
 }
 
-function saveToFile(
+async function getComments(filePath: string): Promise<CommentType[]> {
+  try {
+    const existingComments = await readFromFile(filePath);
+    const fileName = activeEditor.document.fileName;
+    return existingComments.filter((comment) => comment.fileName === fileName);
+  } catch (error) {
+    console.error(`Error getting comments: ${error}`);
+    return [];
+  }
+}
+
+async function showFileComments(filePath: string, context: ExtensionContext) {
+  const fileComments = await getComments(filePath);
+  const iconDecoration: DecorationOptions[] = [];
+  const highlightDecoration: DecorationOptions[] = [];
+
+  fileComments.forEach((comment: CommentType) => {
+    const { start, end } = comment;
+    const startPos = new Position(start.line - 1, start.character - 1);
+    const endPos = new Position(end.line - 1, end.character - 1);
+    const lineLength = activeEditor.document.lineAt(end.line - 1).text.length;
+    const lineStartPosition = new Position(end.line - 1, 0);
+    const lineEndPosition = new Position(end.line - 1, lineLength);
+
+    iconDecoration.push({
+      range: new Range(lineStartPosition, lineEndPosition),
+    });
+    highlightDecoration.push({ range: new Range(startPos, endPos) });
+  });
+
+  const icon = window.createTextEditorDecorationType({
+    after: {
+      contentIconPath: context.asAbsolutePath(path.join("src", "comment.svg")),
+      margin: "5px",
+    },
+  });
+  const highlight = window.createTextEditorDecorationType({
+    backgroundColor: "#8CBEB260",
+  });
+  activeEditor.setDecorations(icon, iconDecoration);
+  activeEditor.setDecorations(highlight, highlightDecoration);
+}
+
+async function readFromFile(filePath: string): Promise<CommentType[]> {
+  try {
+    const jsonData = await fs.promises.readFile(filePath, "utf-8");
+    const { comments } = JSON.parse(jsonData);
+    return comments;
+  } catch (error: any) {
+    if (error.code === "ENOENT") {
+      await fs.promises.writeFile(filePath, '{"comments": []}');
+      return [];
+    } else {
+      console.error(error);
+      return [];
+    }
+  }
+}
+
+async function saveToFile(
+  filePath: string,
   fileName: string,
   selection: Selection,
   commentText: string
 ) {
-  const jsonFilePath = getFilePath();
-
-  if (!jsonFilePath) {
-    window.showErrorMessage("JSON file not found");
-    return;
-  }
-
   try {
     const commentData: CommentType = {
-      fileName: fileName,
+      fileName,
       start: {
         line: selection.start.line + 1,
         character: selection.start.character + 1,
@@ -133,75 +175,21 @@ function saveToFile(
       comment: commentText,
     };
 
-    const dataFromFile = JSON.parse(fs.readFileSync(jsonFilePath, "utf-8"));
-    dataFromFile.comments.push(commentData);
-    const commentJson = JSON.stringify(dataFromFile);
-    fs.writeFileSync(jsonFilePath, commentJson);
+    const existingComments = await readFromFile(filePath);
+    existingComments.push(commentData);
+    const commentJson = JSON.stringify({ comments: existingComments });
+    await fs.promises.writeFile(filePath, commentJson);
   } catch (error) {
-    window.showErrorMessage(`Error saving to file: ${error}`);
-    return;
+    console.error(`Error saving to file: ${error}`);
   }
 }
 
-async function readFromFile() {
-  const jsonFilePath = getFilePath();
-
-  if (!jsonFilePath) {
-    window.showErrorMessage("File not found");
-    return;
-  }
-
-  try {
-    const jsonData = await fs.promises.readFile(jsonFilePath, "utf-8");
-    const commentsFromJson = JSON.parse(jsonData).comments;
-    commentsFromJson.forEach((comment: CommentType) => comments.push(comment));
-  } catch (error: any) {
-    if (error.code === "ENOENT") {
-      fs.writeFileSync(jsonFilePath, '{"comments": []}'); // Makes new file if it doesnt exist
-    } else {
-      console.error(error);
-    }
-  }
-}
-
-async function getComments() {
-  comments = [];
-  iconDecoration = [];
-  highlightDecoration = [];
-  activeEditor.setDecorations(icon, []);
-  activeEditor.setDecorations(highlight, []);
-
-  await readFromFile(); // Adds comments from JSON to array
-
-  const fileName: string = activeEditor.document.fileName;
-  const fileComments = comments.filter(
-    (comment) => comment.fileName === fileName
-  );
-
-  fileComments.forEach((comment: CommentType) => {
-    const { start, end } = comment;
-    const startPos = new Position(start.line - 1, start.character - 1);
-    const endPos = new Position(end.line - 1, end.character - 1);
-    const lineLength = activeEditor.document.lineAt(end.line - 1).text.length;
-    const lineStartPosition = new Position(end.line - 1, 0);
-    const lineEndPosition = new Position(end.line - 1, lineLength);
-    iconDecoration.push({
-      range: new Range(lineStartPosition, lineEndPosition),
-    });
-    highlightDecoration.push({ range: new Range(startPos, endPos) });
-  });
-
-  activeEditor.setDecorations(icon, iconDecoration);
-  activeEditor.setDecorations(highlight, highlightDecoration);
-}
-
-function getFilePath() {
-  const fileName = "comments.json";
+function getFilePath(fileName: string) {
   const docUri = activeEditor.document.uri;
   const workspaceFolder = workspace.getWorkspaceFolder(docUri);
   if (!workspaceFolder) {
     window.showErrorMessage("No workspace folder found.");
-    return;
+    return "";
   }
   return path.join(workspaceFolder.uri.fsPath, fileName);
 }
