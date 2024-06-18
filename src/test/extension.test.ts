5218df5218dc1d4a6898d6cc4aa98ac23d99d21e
KC <kcesmena@stud.ntnu.no>
2024-06-18 12:23:33 +0200

    Create unite test for adding and deleting inline comment

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,128 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+import * as fs from "fs";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { afterEach, beforeEach } from "mocha";
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+import { InlineComment } from "../comment";
+import path from "path";
+import { readFromFile } from "../utils/file-utils";
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+suite("Inline comment tests", () => {
+  const jsonFilePath = path.normalize("inline-comments.json");
+  let comment1: InlineComment;
+
+  // Deletes the JSON file after each test
+  afterEach(() => {
+    if (fs.existsSync(jsonFilePath)) {
+      fs.unlinkSync(jsonFilePath);
+    }
+  });
+
+  // Checks that array of inline comments is empty
+  beforeEach(async () => {
+    const emptyJsonContent = JSON.stringify({ inlineComments: [] });
+    await fs.promises.writeFile(jsonFilePath, emptyJsonContent);
+    const fileData = await readFromFile(jsonFilePath);
+    const comments = fileData.inlineComments;
+    assert.strictEqual(
+      comments.length,
+      0,
+      "The comments array should be empty initially."
+    );
+
+    comment1 = {
+      id: 1,
+      fileName: "file1.ts",
+      start: { line: 1, character: 0 },
+      end: { line: 1, character: 10 },
+      title: "Reusability",
+      comment: "Be more modular",
+      priority: 1,
+    };
+  });
+
+  test("Add new inline comments", async () => {
+    const jsonFileWithOneComment = JSON.stringify({
+      inlineComments: [comment1],
+    });
+    await fs.promises.writeFile(jsonFilePath, jsonFileWithOneComment);
+    let fileData = await readFromFile(jsonFilePath);
+    let comments = fileData.inlineComments;
+
+    assert.strictEqual(
+      comments.length,
+      1,
+      "There should only be one comment in the file."
+    );
+    assert.deepStrictEqual(
+      comments[0],
+      comment1,
+      "The first comment should match the added comment."
+    );
+
+    const comment2: InlineComment = {
+      id: 2,
+      fileName: "file1.ts",
+      start: {
+        line: 2,
+        character: 0,
+      },
+      end: {
+        line: 10,
+        character: 20,
+      },
+      title: "For-loop",
+      comment: "Should be improved for better performance",
+      priority: 3,
+    };
+    const jsonFileWithTwoComments = JSON.stringify({
+      inlineComments: [comment1, comment2],
+    });
+    await fs.promises.writeFile(jsonFilePath, jsonFileWithTwoComments);
+    fileData = await readFromFile(jsonFilePath);
+    comments = fileData.inlineComments;
+
+    assert.strictEqual(
+      comments.length,
+      2,
+      "There should be two comments in the file."
+    );
+    assert.deepStrictEqual(
+      comments[0],
+      comment1,
+      "The first comment should match the added comment."
+    );
+    assert.deepStrictEqual(
+      comments[1],
+      comment2,
+      "The second comment should match the added comment."
+    );
+  });
+
+  test("Delete an inline comment", async () => {
+    const fileWithOneComment = JSON.stringify({ inlineComments: [comment1] });
+    await fs.promises.writeFile(jsonFilePath, fileWithOneComment);
+    let fileData = await readFromFile(jsonFilePath);
+    let comments = fileData.inlineComments;
+
+    const commentIndex = comments.findIndex(
+      (comment: InlineComment) => comment.id === comment1.id
+    );
+    if (commentIndex !== -1) {
+      comments.splice(commentIndex, 1);
+      const updatedJsonContent = { inlineComments: comments };
+      await fs.promises.writeFile(
+        jsonFilePath,
+        JSON.stringify(updatedJsonContent)
+      );
+    }
+
+    fileData = await readFromFile(jsonFilePath);
+    comments = fileData.inlineComments;
+    assert.strictEqual(
+      comments.length,
+      0,
+      "The comments array should be empty after deleting the comment."
+    );
+  });
 });
